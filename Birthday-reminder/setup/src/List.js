943666240bf54060efd46489807900d18e067bce
AnjanaLak <hkanjanalakshan95@gmail.com>
2021-12-18 23:03:19 +0530

    FInalized view component

--- Birthday-reminder/setup/src/List.js
+++ Birthday-reminder/setup/src/List.js
@@ -9,7 +9,8 @@ const List = ({ people }) => {
           <article key={id} className='person'>
             <img src={image} alt={name} />
             <div>
-              <h4></h4>
+              <h4>{name}</h4>
+              <p>{age} years</p>
             </div>
           </article>
         )
--- Birthday-reminder/setup/src/data.js
+++ Birthday-reminder/setup/src/data.js
@@ -1,14 +1,14 @@
 export default [
   {
     id: 1,
-    name: 'Bertie Yates',
+    name: 'Anjana Lakshan',
     age: 29,
     image:
       'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg',
   },
   {
     id: 2,
-    name: 'Hester Hogan',
+    name: 'Saman Gamage',
     age: 32,
     image:
       'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-3_rxtqvi.jpg',
